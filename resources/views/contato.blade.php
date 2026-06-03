886424d3c049c88003b9913b639a8f54a178a5fd
Gustavo Messias de Azevedo <gustavo.mazevedo1@senacsp.edu.br>
2026-06-02 21:39:50 -0300

    first commit

---
+++ resources/views/contato.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.site')
+
+@section('conteudo')
+
+    <div>
+        <h2 class="text-2x1 font-bold">Contato</h2>
+        <p class="text-sm text-slate-600">Estamos a disposição para ouvir você. Se tiver dúvidas, sugestões, críticas, informações para compartilhar com nossa equipe editorial, utilize o formulário abaixo.</p>
+    </div>
+
+
+@endsection
